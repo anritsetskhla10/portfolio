084f48ac2740149f0d2d27bda0a366dfec1313d5
Anri <67234362+anritsetskhla10@users.noreply.github.com>
2024-09-02 02:11:13 +0400

    add animation

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,31 +5,77 @@ import { TbBrandJavascript } from "react-icons/tb";
 import { TiHtml5 } from "react-icons/ti";
 import { SiTailwindcss } from "react-icons/si";
 import { FaBootstrap } from "react-icons/fa";
+import {  motion } from "framer-motion"
+
+const iconVariants = (duration) => ({
+    initial: { y: -10},
+    animate: {
+        y:[ 10 , -10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity, 
+            repeatType: "reverse"
+        }
+    }
+})
 
 function Technologies() {
   return (
     <div className='border-b border-neutral-800 py-24'>
-        <h2 className='my-20 text-center text-4xl'>Technologies</h2>
-        <div className="flex flex-wrap items-center justify-center gap-4">
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+        <motion.h2 
+         whileInView={{opacity: 1, y:0 }}
+         initial={{opacity: 0, y: -100}}
+         transition={{duration: 1.5}}
+        className='my-20 text-center text-4xl'>Technologies</motion.h2>
+        <motion.div 
+         whileInView={{opacity: 1, x:0 }}
+         initial={{opacity: 0, x: -100}}
+         transition={{duration: 1.5}}
+        className="flex flex-wrap items-center justify-center gap-4">
+            <motion.div 
+            variants={iconVariants(2.5)}
+            initial='initial'
+            animate='animate'
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <RiReactjsLine className='text-7xl text-cyan-400'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+             variants={iconVariants(3)}
+             initial='initial'
+             animate='animate'
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <TbBrandTypescript className='text-7xl text-blue-400'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+             variants={iconVariants(5)}
+             initial='initial'
+             animate='animate'
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <TbBrandJavascript className='text-7xl text-yellow-500'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+             variants={iconVariants(2)}
+             initial='initial'
+             animate='animate'
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <TiHtml5 className='text-7xl text-orange-400'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+             variants={iconVariants(6)}
+             initial='initial'
+             animate='animate'
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <SiTailwindcss  className='text-7xl text-cyan-600'/>
-            </div>
-            <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+            </motion.div>
+            <motion.div 
+             variants={iconVariants(4)}
+             initial='initial'
+             animate='animate'
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <FaBootstrap  className='text-7xl text-purple-600'/>
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
       
     </div>
   )
